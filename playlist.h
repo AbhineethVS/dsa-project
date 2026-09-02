1373b16b6199de2aff53118c1606aa67005c1fb7
Vaisakh <260074834+vaisakh-collab@users.noreply.github.com>
2026-09-02 15:10:55 +0530

    Updated everything

---
+++ playlist.h
@@ -0,0 +1,30 @@
+#ifndef PLAYLIST_H
+#define PLAYLIST_H
+
+typedef struct Song Song;
+
+void playSong();
+
+void pauseSong();
+
+void resumeSong();
+
+void nextSong();
+
+void previousSong();
+
+int viewCurrentSong();
+
+void enqueuePlayNext();
+
+void dequeuePlayNext();
+
+void clearPlayNext();
+
+void pushRecentlyPlayed();
+
+int popRecentlyPlayed();
+
+int isPlaying();
+
+#endif
